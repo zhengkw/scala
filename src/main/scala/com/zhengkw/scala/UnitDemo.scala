6c453b126add78a94bb86d36f19c597babcfb4d3
zhengkw <swimer@vip.qq.com>
2020-04-20 21:01:02 +0800

    Unit

---
+++ src/main/scala/com/zhengkw/scala/UnitDemo.scala
@@ -0,0 +1,17 @@
+package com.zhengkw.scala
+
+/**
+ * @ClassName:UnitDemo
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/20下午 8:31
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object UnitDemo {
+  def main(args: Array[String]): Unit = {
+    //测试Unit返回值是什么
+    var a:Unit =123
+    println(a) //返回 ()
+  }
+}
