8d848b9d97eee580d8e9e373a33f6f60450affb6
zhengkw <swimer@vip.qq.com>
2020-04-20 14:06:01 +0800

    commit

---
+++ src/main/scala/com/zhengkw/scala/helloworld.scala
@@ -0,0 +1,16 @@
+package com.zhengkw.scala
+
+object helloworld {
+  /**
+   * @descrption:
+   * @param args
+   * @return: void
+   * @date: 20/04/20 上午 11:07
+   * @author: zhengkw
+   */
+  def main(args: Array[String]): Unit = {
+    System.out.println("java")
+    println("scala")
+  }
+
+}
