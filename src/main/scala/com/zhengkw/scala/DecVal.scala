51fe66a460d58d2e3eab86bad94ceff7b18dc308
zhengkw <swimer@vip.qq.com>
2020-04-20 21:08:07 +0800

    声明变量

---
+++ src/main/scala/com/zhengkw/scala/DecVal.scala
@@ -0,0 +1,13 @@
+package com.zhengkw.scala
+
+/**
+ * @ClassName:DecVal
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/20下午 9:06
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object DeclaraVal {
+
+}
