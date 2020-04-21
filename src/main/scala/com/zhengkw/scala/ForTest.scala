ae1f947af08355c370bd5733b8c4fe81272c7314
zhengkw <swimer@vip.qq.com>
2020-04-21 11:27:28 +0800

    change name

--- src/main/scala/com/zhengkw/scala/ForTest.scala
+++ src/main/scala/com/zhengkw/scala/ForTest.scala
@@ -9,5 +9,22 @@ package com.zhengkw.scala
  * @since: jdk 1.8 scala 2.11.8
  */
 object ForTest {
-
+  def main(args: Array[String]): Unit = {
+    var s="hi zhengkw where are you from"
+    for(c<-s){
+      print(c +'\t')
+    }
+    println("-------1-----------")
+  for ( i<- 1 to 100 by 2) println(i)
+  println("--------2----------")
+  for ( i<- 1 to (100,5)) println(i)
+}
+  var m=10
+  while (m>0 ){
+    m-=1
+    println(m)
+  }
+  println("--------3----------")
+  //判断是偶数执行循环
+  for(i<- 0 until 99 by 1 if i%2==0) System.out.println(i)
 }
