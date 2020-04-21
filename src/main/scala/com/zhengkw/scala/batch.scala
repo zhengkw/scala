70c56770108231c7d8ca0835e30206cc9b09eb61
zhengkw <swimer@vip.qq.com>
2020-04-21 10:00:01 +0800

    raw test

---
+++ src/main/scala/com/zhengkw/scala/batch.scala
@@ -0,0 +1,25 @@
+package com.zhengkw.scala
+
+/**
+ * @ClassName:batch
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/21上午 9:35
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object batch {
+  def main(args: Array[String]): Unit = {
+
+    var m=10
+    var n =2
+
+
+    val max = if (m > n) m else n
+    println(max)
+    println(m=n)
+    println(m=30)
+    println(m)
+  }
+
+}
