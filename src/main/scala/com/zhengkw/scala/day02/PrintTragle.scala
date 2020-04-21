137d8de1bdd6b6d6aa35888086a98db6c92cb4cd
zhengkw <swimer@vip.qq.com>
2020-04-22 00:29:23 +0800

    输出三角形

--- src/main/scala/com/zhengkw/scala/day02/PrintTragle.scala
+++ src/main/scala/com/zhengkw/scala/day02/PrintTragle.scala
@@ -1,23 +1,61 @@
 package com.zhengkw.scala.day02
 
+import scala.io.StdIn
+
 /**
  * @ClassName:PrintTragle
  * @author: zhengkw
- * @description:
+ * @description:输出一个三角形
  * @date: 20/04/21下午 10:49
  * @version:1.0
  * @since: jdk 1.8 scala 2.11.8
  */
 object PrintTragle {
   def main(args: Array[String]): Unit = {
+    var flag = true
 
-  }
-  def printTragle(line:Int){
-    for(i<- 1 to line){
-      for(j<-line to i){
-
+    while (flag) {
+      val str = StdIn.readLine("需要打印多少行,输入-1结束循环")
+      try {
+        if (str.toInt > 0) {
+          lineNum = str.toInt
+          printTragle(lineNum)
+        } else println("已退出")
+      } catch {
+        case e => flag = false
       }
     }
+  }
+
+  var lineNum = 0
+
+  /**
+   * @descrption: 打印方法
+   * @param line
+   * @return: void
+   * @date: 20/04/21 下午 11:52
+   * @author: zhengkw
+   */
+  def printTragle(line: Int) {
+    //输出 line-1行
+    for (i <- 1 until line) {
+      var count = 0 //输出空格个数
+      //控制每行输出
+      for (m <- 1 to line - i) {
+        //输出空格
+        print(" ")
+        count += 1
+      }
+      //输出该行的*等于总行数-空格数
+      for (n <- 1 to line - count) print("* ")
+      //输出完一行
+      println()
+    }
+    //打印最后一行
+    for (i <- 1 to line) print("* ")
+
+    //打印换行为了后续继续输入
+    println()
 
   }
 }
