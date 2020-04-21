049497160aa70621805134302a48d27f81c77dc6
zhengkw <swimer@vip.qq.com>
2020-04-21 13:51:58 +0800

    change string

--- src/main/scala/com/zhengkw/scala/ForTest.scala
+++ src/main/scala/com/zhengkw/scala/ForTest.scala
@@ -1,4 +1,5 @@
 package com.zhengkw.scala
+
 import scala.util.control.Breaks
 
 /**
@@ -11,35 +12,38 @@ import scala.util.control.Breaks
  */
 object ForTest {
   def main(args: Array[String]): Unit = {
-    var s="zhengkw"
-    for(c <- s){
-      print(c +'\t')
+    var s = "zhengkw"
+    for (c <- s) {
+      print(c + '\t') //错误写法 这个是char整型的加法运算
+      println
+      println(c + "\t") // 将C转换成字符串！
     }
     println("-------1-----------")
-  for ( i<- 1 to 100 by 2) println(i)
-  println("--------2----------")
-  for ( i<- 1 to (100,5)) println(i)
-}
-  var m=10
-  while (m>0 ){
-    m-=1
+    for (i <- 1 to 100 by 2) println(i)
+    println("--------2----------")
+    for (i <- 1 to(100, 5)) println(i)
+  }
+
+  var m = 10
+  while (m > 0) {
+    m -= 1
     println(m)
   }
   println("--------3----------")
   //判断是偶数执行循环 循环守卫
-  for(i<- 0 until 99 by 1 if i%2==0) System.out.println(i)
+  for (i <- 0 until 99 by 1 if i % 2 == 0) System.out.println(i)
   println("--------4----------")
- // private val str: String = StdIn.readLine()
-var str="25"
-  var flag=true
-  Breaks.breakable{
-    for(i<- 2 to str.toInt){
-      if(str.toInt % i==0){
-        flag=false
+  // private val str: String = StdIn.readLine()
+  var str = "25"
+  var flag = true
+  Breaks.breakable {
+    for (i <- 2 to str.toInt) {
+      if (str.toInt % i == 0) {
+        flag = false
         Breaks.break
       }
     }
   }
-  println(if(flag) str+"\t是Z数"else str+"\t is 合数")
+  println(if (flag) str + "\t是Z数" else str + "\t is 合数")
 
 }
