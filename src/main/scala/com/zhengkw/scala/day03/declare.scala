da1bfef820e384275977a4366ff0495310ca4e7c
zhengkw <swimer@vip.qq.com>
2020-04-23 21:41:31 +0800

    测试用

---
+++ src/main/scala/com/zhengkw/scala/day03/declare.scala
@@ -0,0 +1,13 @@
+package com.zhengkw.scala.day03
+
+/**
+ * @ClassName:decalr
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/23下午 9:38
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object declare {
+
+}
