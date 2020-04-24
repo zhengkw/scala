e4a11abb556e4036a6e0013ce9488a0f6ebb8ba9
zhengkw <swimer@vip.qq.com>
2020-04-24 19:07:53 +0800

    ♦

--- src/main/scala/com/zhengkw/scala/day04Exc/Poker.scala
+++ src/main/scala/com/zhengkw/scala/day04Exc/Poker.scala
@@ -19,18 +19,19 @@ object Poker {
     val poker = new BasePoker
     poker.numb = (Math.random() * 13 + 1).toInt
     var suitNum = (Math.random() * 4 + 1).toInt
-    if (suitNum == 1) poker.suit = "黑桃"
-    else if (suitNum == 2) poker.suit = "梅花"
-    else if (suitNum == 3) poker.suit = "红桃"
-    else poker.suit = "方片"
+    if (suitNum == 1) poker.suit = "♠"
+    else if (suitNum == 2) poker.suit = "♣"
+    else if (suitNum == 3) poker.suit = "♥"
+    else poker.suit = "♦"
     println(poker.toString)
     poker.whatColor
+    poker.whetherRed
   }
 }
 
 /**
  * @descrption:
- * @param suit 花色 方块 黑桃 红心 梅花
+ * @param suit 花色   ♣,♦,♥,♠，
  * @return:
  * @date: 20/04/24 下午 6:19
  * @author: zhengkw
@@ -44,9 +45,16 @@ class BasePoker {
   }
 
   def whatColor: Unit = {
-    if (suit == "黑桃") println("颜色是黑色")
-    else if (suit == "梅花") println("颜色是黑色")
-    else if (suit == "红桃") println("颜色是红色")
+    if (suit == "♠") println("颜色是黑色")
+    else if (suit == "♣") println("颜色是黑色")
+    else if (suit == "♥") println("颜色是红色")
     else println("颜色是红色")
+  }
+
+  def whetherRed: Unit = {
+    if (suit == "♠") println("不是红色")
+    else if (suit == "♣") println("不是红色")
+    else if (suit == "♥") println("是红色")
+    else println("是红色")
   }
 }
