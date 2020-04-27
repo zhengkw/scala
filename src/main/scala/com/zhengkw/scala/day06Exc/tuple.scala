ba3569a32097772948e01aededf624023016c15a
zhengkw <swimer@vip.qq.com>
2020-04-27 23:52:36 +0800

    tuple

---
+++ src/main/scala/com/zhengkw/scala/day06Exc/tuple.scala
@@ -0,0 +1,22 @@
+package com.zhengkw.scala.day06Exc
+
+/**
+ * @ClassName:tuple
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/27下午 11:48
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object tuple {
+  def main(args: Array[String]): Unit = {
+    val tuple1 = tuple./%(4, 2)
+    println(tuple1._1)
+    println(tuple1._2)
+  }
+
+  def /%(a: Int, b: Int): (Int, Int) = {
+    (a / b, a % b)
+
+  }
+}
