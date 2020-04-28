cd714fb4d94a4b11b63cfb316c130b332a32f3bd
zhengkw <swimer@vip.qq.com>
2020-04-28 11:52:27 +0800

    map demo2

--- src/main/scala/com/zhengkw/scala/day07/MapDemo2.scala
+++ src/main/scala/com/zhengkw/scala/day07/MapDemo2.scala
@@ -1,5 +1,7 @@
 package com.zhengkw.scala.day07
 
+import scala.collection.mutable
+
 /**
  * @ClassName:MapDemo2
  * @author: zhengkw
@@ -11,5 +13,13 @@ package com.zhengkw.scala.day07
 object MapDemo2 {
   def main(args: Array[String]): Unit = {
     val map = Map("a" -> 97, "b" -> 98, "c" -> 99)
+    println(map("a"))
+    //  println(map("d"))
+    println(map.get("d"))
+    println(map.getOrElse("d", 100))
+    println(map)
+    val map1 = mutable.Map("a" -> 97, "b" -> 98, "c" -> 99)
+    map1.getOrElseUpdate("d",100)
+    println(map1)
   }
 }
