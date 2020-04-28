bb0e8a18feaf1d6d7dff59020d969f237e3d4d36
zhengkw <swimer@vip.qq.com>
2020-04-28 10:30:45 +0800

    map demo2

---
+++ src/main/scala/com/zhengkw/scala/day07/MapDemo.scala
@@ -0,0 +1,45 @@
+package com.zhengkw.scala.day07
+
+/**
+ * @ClassName:MapDemo
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/28上午 10:21
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object MapDemo {
+  def main(args: Array[String]): Unit = {
+    //   / val map = Map((2, "234"), (2, "13"))
+    val map = Map(("a", 97), ("b", 98), ("c", 98))
+    //遍历
+    val k = map.keySet
+    println(k)
+    for (elem <- k) {
+      //遍历map
+      println(map(elem))
+    }
+    //遍历
+    for (kv <- map) {
+      println(kv._1 + " " + kv._2)
+    }
+    //遍历
+    for ((k, v) <- map) {
+      println(k + " " + v)
+    }
+    //遍历v为98的key
+    for ((k, 98) <- map) {
+      println(k)
+    }
+    println("-----遍历K0-----")
+    for ((k, _) <- map) {
+      println(k)
+    }
+
+    for (k <- map.keySet) {
+      println(k)
+    }
+  }
+
+
+}
