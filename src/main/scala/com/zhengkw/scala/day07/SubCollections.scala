a352c6dbc08a7e5a04c38f99d3389f55757e24f2
zhengkw <swimer@vip.qq.com>
2020-04-28 11:55:51 +0800

    common

---
+++ src/main/scala/com/zhengkw/scala/day07/SubCollections.scala
@@ -0,0 +1,40 @@
+package com.zhengkw.scala.day07
+
+/**
+ * @ClassName:SubCollections
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/28上午 11:53
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object SubCollections {
+  def main(args: Array[String]): Unit = {
+//            （1）获取集合的头head
+
+//            （2）获取集合的尾（不是头就是尾）tail
+
+//            （3）集合最后一个数据 last
+
+//            （4）集合初始数据（不包含最后一个）
+
+//            （5）反转
+
+//            （6）取前（后）n个元素
+
+//            （7）去掉前（后）n个元素
+
+//            （8）并集
+
+//            （9）交集
+
+//            （10）差集
+
+//            （11）拉链
+
+//            （12）滑窗
+    val list1 = List(30, 50, 70, 60, "abc")
+    val list2 = List("30", "50", "7", "6", "10", "2", "abc")
+
+  }
+}
