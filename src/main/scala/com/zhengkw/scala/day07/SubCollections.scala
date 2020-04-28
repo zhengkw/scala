35615f1aa1ccccb5cebb72aec001fcc49ec6a822
zhengkw <swimer@vip.qq.com>
2020-04-28 12:07:32 +0800

    common

--- src/main/scala/com/zhengkw/scala/day07/SubCollections.scala
+++ src/main/scala/com/zhengkw/scala/day07/SubCollections.scala
@@ -8,33 +8,39 @@ package com.zhengkw.scala.day07
  * @version:1.0
  * @since: jdk 1.8 scala 2.11.8
  */
-object SubCollections {
-  def main(args: Array[String]): Unit = {
-//            （1）获取集合的头head
-
-//            （2）获取集合的尾（不是头就是尾）tail
-
-//            （3）集合最后一个数据 last
-
-//            （4）集合初始数据（不包含最后一个）
-
-//            （5）反转
-
-//            （6）取前（后）n个元素
-
-//            （7）去掉前（后）n个元素
-
-//            （8）并集
-
-//            （9）交集
-
-//            （10）差集
-
-//            （11）拉链
-
 //            （12）滑窗
-    val list1 = List(30, 50, 70, 60, "abc")
-    val list2 = List("30", "50", "7", "6", "10", "2", "abc")
+object SubCollections {
+  val list1 = List(30, 50, 70, 60, "abc")
+  val list2 = List("30", "50", "7", "6", "10", "2", "abc")
+
+  def main(args: Array[String]): Unit = {
+    //            （1）获取集合的头head
+    println(list1.head)
+    //            （2）获取集合的尾（不是头就是尾）tail
+    println(list1.tail)
+    //            （3）集合最后一个数据 last
+    println(list1.last)
+    //            （4）集合初始数据（不包含最后一个）
+    println(list1.init)
+    val inits = list1.inits
+    println("组合")
+    inits.toList.foreach(println)
+    //            （5）反转
+    println(list1.reverse)
+    //            （6）取前（后）n个元素
+    println(list1.take(2))
+    println(list1.takeRight(2))
+    //            （7）去掉前（后）n个元素
+    println(list1.drop(2))
+    println(list1.dropRight(2))
+    //            （8）并集
+    println(list1.union(list2))
+    //            （9）交集
+    println(list1.intersect(list2))
+    //            （10）差集
+    println(list1.diff(list2))
+    //            （11）拉链
+    println(list1.zipAll(list2, -1, -2))
 
   }
 }
