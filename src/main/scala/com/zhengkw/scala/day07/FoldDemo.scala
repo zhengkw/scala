9ca227e61ef1f4c910f22f1338a157ac14e6ccc7
zhengkw <swimer@vip.qq.com>
2020-04-28 22:11:45 +0800

    fold

---
+++ src/main/scala/com/zhengkw/scala/day07/FoldDemo.scala
@@ -0,0 +1,20 @@
+package com.zhengkw.scala.day07
+
+/**
+ * @ClassName:FoldDemo
+ * @author: zhengkw
+ * @description:
+ * @date: 20/04/28下午 9:50
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object FoldDemo {
+  def main(args: Array[String]): Unit = {
+    val list1 = List(30, 50, 70, 60, 10, 20)
+    //list1.foldLeft(0)((x: Int, y: Int) => x + y)
+    //list1.foldLeft(0)((x, y) => x + y)
+    // list1.sum
+    println(list1.fold(0)(_ + _))
+
+  }
+}
