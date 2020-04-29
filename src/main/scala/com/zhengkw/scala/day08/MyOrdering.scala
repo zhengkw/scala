993dcc00b83a35ca97f5dbff8a89b063f675b956
zhengkw <swimer@vip.qq.com>
2020-04-29 14:08:05 +0800

    Ordering

--- src/main/scala/com/zhengkw/scala/day08/MyOrdering.scala
+++ src/main/scala/com/zhengkw/scala/day08/MyOrdering.scala
@@ -20,7 +20,8 @@ object MyOrdering {
     }
     val users = List(user1, user2, user3)
     println(users.sorted)
-    val list = users.sortBy(user => (user.age, user.name.length))(Ordering.Tuple2(Ordering.Int.reverse, Ordering.Int.reverse))
+   // val list = users.sortBy(user => (user.age, user.name.length))(Ordering.Tuple2(Ordering.Int.reverse, Ordering.Int.reverse))
+    val list = users.sortBy(user => (-user.age, -user.name.length))
     println(list)
   }
 }
